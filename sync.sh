b5100598e2f35745cad84ef6a041d0d5419d7b81
maxx0021 <100127589+maxx0021@users.noreply.github.com>
2023-03-23 22:04:49 +0530

    [skip ci]

--- sync.sh
+++ sync.sh
@@ -18,13 +18,7 @@ git clone --depth=1 $VT_LINK $VT_PATH
 git clone --depth=1 $KT_LINK $KT_PATH
 
 # Custom
-rm -rf hardware/xiaomi
-rm -rf hardware/qcom-caf/wlan
-rm -rf hardware/qcom-caf/bootctrl
-rm -rf hardware/google/pixel/kernel_headers
-git clone --depth=1 https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-20 hardware/xiaomi
-git clone --depth=1 https://github.com/Jabiyeff-Project/android_hardware_qcom_wlan.git -b 13.0-spes hardware/qcom-caf/wlan
-git clone --depth=1 https://github.com/ArrowOS/android_hardware_qcom_bootctrl.git -b arrow-13.0-caf hardware/qcom-caf/bootctrl
+
 
 # Exit
 exit 0
